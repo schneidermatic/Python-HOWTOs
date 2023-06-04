38239cc1b0b8ccbb7b50bbd7b7e4a03dbd748f99
schneidermatic <markus.schneider73@gmail.com>
2023-06-04 12:33:25 +0200

    * Updated - A bunch of files were modified.
     Changes to be committed:
    	renamed:    .pyapprc -> .appenv
    	renamed:    .xrc -> .apprc
    	modified:   src/logger.py

--- recipe-005/src/logger.py
+++ recipe-005/src/logger.py
@@ -2,6 +2,7 @@
 
 import time
 import logging
+from sys import stdout
 
 class Logger():
 
@@ -14,6 +15,6 @@ class Logger():
         
     def set_logger(self, logger_name):
         numeric_loglevel = getattr(logging, self.args.loglevel.upper(), None)
-        logging.basicConfig(level=numeric_loglevel,format=self.config.log_format,datefmt=self.config.log_date_format)
+        logging.basicConfig(stream=stdout,level=numeric_loglevel,format=self.config.log_format,datefmt=self.config.log_date_format)
         logging.Formatter.converter = time.gmtime
         self.logger = logging.getLogger(logger_name)
